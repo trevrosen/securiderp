44f5851f40c4320bec33733152c337f904994644
Trevor Rosen <trevor@catapult-creative.com>
2015-06-24 11:50:44 -0500

    Add basic file to hold content

---
+++ name_generator.rb
@@ -0,0 +1,76 @@
+class NameGenerator
+  NOUNS = {
+    serious: [
+      "Threat",
+      "Thunder",
+      "Mach",
+      "Strike",
+      "Defender",
+      "Defense",
+      "Metal",
+      "Eagle",
+      "Attack",
+      "Vigil",
+      "Penetration",
+      "Network",
+      "Perimeter",
+      "Security",
+      "Falcon",
+      "Panzer",
+      "Ninja",
+      "Fortress",
+      "Sword",
+      "Armor",
+      "Crypt",
+      "Crypto",
+      "Core",
+      "Talon",
+      "Knife",
+      "Hawk",
+      "Raptor"
+    ],
+
+    joke: [
+      "Voltron",
+      "Molester",
+      "Corpse"
+    ]
+  }
+
+  ADJECTIVES = {
+    serious: [
+      "Cyber",
+      "Cyborg",
+      "Cobalt",
+      "Red",
+      "Black",
+      "Gray",
+      "Steel",
+      "Vigilant",
+      "Active",
+    ],
+
+    joke: [
+      "Slammin",
+      "Gorey"
+    ]
+  }
+
+
+  #PREFIXES = {
+    #serious: [
+    #],
+
+    #joke: [
+    #]
+  #}
+
+  SUFFIXES = {
+    serious: [
+      "Ops"
+    ],
+
+    #joke: [
+    #]
+  }
+end
